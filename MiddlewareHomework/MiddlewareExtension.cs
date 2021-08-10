534a3487b41741a9bf1f5e3f6f99d394f857062a
антон сизов <aantonssizov@gmail.com>
2021-08-10 20:06:07 +0300

    First commit

---
+++ MiddlewareHomework/MiddlewareExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiddlewareHomework
+{
+    public static class MiddlewareExtension
+    {
+        public static IApplicationBuilder UseCustomMiddleware(this IApplicationBuilder applicationBuilder, string path)
+        {
+            return applicationBuilder.Map(path, b => b.UseMiddleware<PublishMiddleware>());
+        }
+    }
+}
---
+++ MiddlewareHomework/PublishMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Globalization;
+using System.Threading.Tasks;
+using Interfaces;
+
+namespace MiddlewareHomework
+{
+    public class PublishMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public PublishMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context, ISaveInfoService saveInfo, IUploadArticleService uploadArticle)
+        {
+            saveInfo.SaveInfo();
+            uploadArticle.UploadArticle();
+            await context.Response.WriteAsync("Published Article");
+            await _next.Invoke(context);
+        }
+    }
+}
